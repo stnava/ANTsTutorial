6d00abb21a920239126029365dc0e9fc15fd10c2
stnava <stnava@gmail.com>
2015-07-27 12:57:01 -0700

    WIP: testing more

--- test.R
+++ test.R
@@ -1,7 +1,8 @@
 library(rmarkdown)
 fns=Sys.glob("src/*Rmd")
-fns=c(".//src/phantomMorphometryStudy.Rmd",
-      ".//src/rfLesionSeg.Rmd")
+fns=c("./src/phantomMorphometryStudy.Rmd",
+      "./src/rfLesionSeg.Rmd",
+      "./src/ANTsR_Intro.Rmd")
 for ( fn in fns )
   {
   render( fn ,"pdf_document")
