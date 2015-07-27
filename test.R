4e1ed7bcabb931356f75b3f2af38f8e8e0bf320d
stnava <stnava@gmail.com>
2015-07-27 13:27:32 -0700

    COMP: wrong output type

--- test.R
+++ test.R
@@ -4,8 +4,8 @@ fns=c("./src/phantomMorphometryStudy.Rmd",
       "./src/rfLesionSeg.Rmd",
       "./src/ANTsR_Intro.Rmd")
 rm(list = ls()) 
-render( "./src/phantomMorphometryStudy.Rmd" , "pdf_document")
+render( "./src/phantomMorphometryStudy.Rmd")
 rm(list = ls())
-render( "./src/rfLesionSeg.Rmd" ,"pdf_document")
+render( "./src/rfLesionSeg.Rmd")
 rm(list = ls())
-render( "./src/ANTsR_Intro.Rmd" ,"pdf_document")
+render( "./src/ANTsR_Intro.Rmd")
